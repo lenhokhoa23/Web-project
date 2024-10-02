7f4ac893d326c6dd5a8c61ba76a4f4893e2b40f0
MagicianFrog <khang.na2005@gmail.com>
2024-10-02 20:56:26 +0700

    Create EmployeeHealth table

--- company_sample.sql
+++ company_sample.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost:3307
--- Thời gian đã tạo: Th10 02, 2024 lúc 11:41 AM
+-- Thời gian đã tạo: Th10 02, 2024 lúc 03:55 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.0.30
 
@@ -129,6 +129,63 @@ CREATE TABLE `employeehealth` (
   `Weight` decimal(5,2) NOT NULL,
   `Status` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `employeehealth`
+--
+
+INSERT INTO `employeehealth` (`Employee_ID`, `EmployeeName`, `Gender`, `Height`, `Weight`, `Status`) VALUES
+(1, 'Nguyễn Văn Minh', 'Nam', 175.50, 70.00, 'Bình thường'),
+(2, 'Lê Doãn Khế', 'Nam', 180.20, 75.50, 'Ốm'),
+(3, 'Trần Thị Mai', 'Nữ', 160.30, 55.00, 'Bình thường'),
+(4, 'Phạm Ngọc Huy', 'Nam', 170.80, 68.20, 'Bình thường'),
+(5, 'Ngô Văn An', 'Nam', 175.00, 72.00, 'Bình thường'),
+(6, 'Bùi Thị Bích', 'Nữ', 165.00, 58.00, 'Bình thường'),
+(7, 'Đặng Văn Hòa', 'Nam', 178.50, 80.00, 'Bình thường'),
+(8, 'Vũ Thị Huệ', 'Nữ', 162.20, 57.00, 'Bình thường'),
+(9, 'Lê Hoàng', 'Nam', 182.00, 77.00, 'Bình thường'),
+(10, 'Nguyễn Tuấn', 'Nam', 170.00, 70.50, 'Bình thường'),
+(11, 'Phan Thị Thảo', 'Nữ', 160.00, 55.50, 'Bình thường'),
+(12, 'Nguyễn Văn Tâm', 'Nam', 175.20, 72.00, 'Ốm'),
+(13, 'Lê Hoàng Hải', 'Nam', 178.00, 78.50, 'Bình thường'),
+(14, 'Trần Minh Tâm', 'Nam', 172.00, 70.00, 'Bình thường'),
+(15, 'Nguyễn Ngọc Sơn', 'Nam', 180.50, 75.00, 'Bình thường'),
+(16, 'Ngô Thị Phương', 'Nữ', 165.50, 59.00, 'Bình thường'),
+(17, 'Bùi Văn Khoa', 'Nam', 177.50, 73.50, 'Bình thường'),
+(18, 'Trần Thị Hương', 'Nữ', 160.50, 55.50, 'Bình thường'),
+(19, 'Nguyễn Văn Lợi', 'Nam', 175.00, 72.50, 'Bình thường'),
+(20, 'Phạm Văn Long', 'Nam', 172.50, 69.00, 'Bình thường'),
+(21, 'Lê Văn Hậu', 'Nam', 178.50, 73.50, 'Bình thường'),
+(22, 'Trần Minh Quân', 'Nam', 175.00, 70.00, 'Bình thường'),
+(23, 'Nguyễn Thị Thúy', 'Nữ', 162.00, 58.00, 'Bình thường'),
+(24, 'Ngô Văn Bảo', 'Nam', 172.50, 72.50, 'Bình thường'),
+(25, 'Bùi Thị Kim', 'Nữ', 175.80, 74.50, 'Bình thường'),
+(26, 'Đặng Văn Phúc', 'Nam', 178.50, 73.50, 'Bình thường'),
+(27, 'Vũ Thị Nga', 'Nữ', 162.00, 58.00, 'Bình thường'),
+(28, 'Trần Văn Hùng', 'Nam', 172.50, 72.50, 'Ốm'),
+(29, 'Nguyễn Văn Quốc', 'Nam', 175.00, 70.00, 'Bình thường'),
+(30, 'Phạm Thị Yến', 'Nữ', 160.30, 55.50, 'Bình thường'),
+(31, 'Lê Văn Thắng', 'Nam', 177.50, 65.00, 'Bình thường'),
+(32, 'Trần Thị Kim', 'Nữ', 165.50, 59.00, 'Bình thường'),
+(33, 'Nguyễn Văn Phúc', 'Nam', 178.00, 77.00, 'Ốm'),
+(34, 'Ngô Thị Thanh', 'Nữ', 160.50, 57.50, 'Bình thường'),
+(35, 'Bùi Văn Tài', 'Nam', 180.20, 76.50, 'Ốm'),
+(36, 'Đặng Văn Bằng', 'Nam', 175.50, 74.00, 'Bình thường'),
+(37, 'Vũ Thị Tuyết', 'Nữ', 162.50, 58.50, 'Bình thường'),
+(38, 'Trần Văn Lâm', 'Nam', 177.00, 71.00, 'Bình thường'),
+(39, 'Nguyễn Thị Thanh', 'Nữ', 172.00, 70.50, 'Bình thường'),
+(40, 'Phạm Văn Nam', 'Nam', 180.00, 76.00, 'Bình thường'),
+(41, 'Lê Doãn Minh', 'Nam', 160.50, 57.50, 'Bình thường'),
+(42, 'Trần Thị Hằng', 'Nữ', 175.00, 72.00, 'Bình thường'),
+(43, 'Nguyễn Văn Vinh', 'Nam', 180.50, 75.50, 'Bình thường'),
+(44, 'Ngô Thị Hạnh', 'Nữ', 178.20, 74.50, 'Bình thường'),
+(45, 'Bùi Văn Khải', 'Nam', 165.50, 58.00, 'Bình thường'),
+(46, 'Đặng Văn Chương', 'Nam', 175.20, 72.80, 'Bình thường'),
+(47, 'Vũ Thị Kim Anh', 'Nữ', 160.20, 56.50, 'Ốm'),
+(48, 'Trần Văn Tuấn', 'Nam', 172.80, 70.50, 'Bình thường'),
+(49, 'Nguyễn Thị Yến', 'Nữ', 160.50, 57.50, 'Bình thường'),
+(50, 'Phạm Văn Kiên', 'Nam', 175.50, 74.00, 'Bình thường'),
+(51, 'Âu Dương Hiệp Hào', 'Nam', 190.00, 75.00, 'Bình thường');
 
 -- --------------------------------------------------------
 
