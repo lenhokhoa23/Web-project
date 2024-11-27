346ced4d9a127eac12c73f2493591f71fe5e3422
lenhokhoa23 <nkhoabn2312@gmail.com>
2024-11-27 21:41:33 +0700

    fix conflict

--- webnodejs/routes/EmployeeRoutes.js
+++ webnodejs/routes/EmployeeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const EmployeeController = require('../controllers/EmployeeController');
+const EmployeeController = require('../controllers/employeeController');
 
 router.get('/employee', EmployeeController.showEmployeeList);
 router.get('/employee/add', EmployeeController.showAddEmployeeForm);
