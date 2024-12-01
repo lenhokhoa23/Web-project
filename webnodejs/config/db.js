bee350e65e0944f73ba6d68287cc970ce751c1a3
lenhokhoa23 <nkhoabn2312@gmail.com>
2024-12-01 22:13:07 +0700

    them file vercel.json

--- webnodejs/config/db.js
+++ webnodejs/config/db.js
@@ -1,12 +1,17 @@
+require('dotenv').config(); 
+
+
+
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'company_sample',
-    port: 3307,
-    multipleStatements: true
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT), // Chuyển về số
+    multipleStatements: process.env.DB_MULTIPLE_STATEMENTS === 'true' // Chuyển về boolean
 });
 
 db.connect((err) => {
